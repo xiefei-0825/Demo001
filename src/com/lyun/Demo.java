56e3b3423b666ebf371a4857c9086530182b877e
xiefei-0825 <1055960947@qq.com>
2022-11-01 21:17:17 +0800

    第五次测试

--- src/com/lyun/Demo.java
+++ src/com/lyun/Demo.java
@@ -4,5 +4,6 @@ public class Demo {
     public static void main(String[] args) {
         System.out.println("好好学习 天天向上");
         System.out.println("Good");
+        System.out.println("Moring");
     }
 }
