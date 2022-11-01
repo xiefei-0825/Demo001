0329b116712560c1da2d9285757f0d5f2dfe3f63
xiefei-0825 <1055960947@qq.com>
2022-11-01 21:36:21 +0800

    第八次测试

--- src/com/lyun/Demo.java
+++ src/com/lyun/Demo.java
@@ -2,12 +2,11 @@ package com.lyun;
 
 public class Demo {
     public static void main(String[] args) {
-<<<<<<< HEAD
+
         System.out.println("加油努力！");
-=======
         System.out.println("好好学习 天天向上");
         System.out.println("Good");
         System.out.println("Moring");
->>>>>>> 333
+
     }
 }
--- src/com/lyun/Text.java
+++ src/com/lyun/Text.java
@@ -4,5 +4,9 @@ public class Text {
     public static void main(String[] args) {
         System.out.println("Hello World!!");
         System.out.println("你好 世界!!");
+        System.out.println("测试4");
+
+
+
     }
 }
